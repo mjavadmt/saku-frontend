3c673a31c9e58d25b43dab2d46086bc14265d7b0
ali6600alamdari <ali.alamdari9088@gmail.com>
2022-03-20 11:05:52 +0330

    fix:filteringAuction

--- src/pages/myAuctions.jsx
+++ src/pages/myAuctions.jsx
@@ -14,6 +14,36 @@ export const MyAuctions = () => {
         remainingDay="20"
         tags={["عمرانی", "فوری", "پیش پرداختی", "دولتی"]}
       />
+      <ActionCard
+        imgSrc="https://images.unsplash.com/photo-1612151855475-877969f4a6cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+        title="مزایده اول"
+        city="تهران"
+        companyName="وزارت نیرو"
+        date="1401/05/02"
+        price="500,000,000"
+        remainingDay="20"
+        tags={["عمرانی", "فوری", "پیش پرداختی", "دولتی"]}
+      />
+      <ActionCard
+        imgSrc="https://images.unsplash.com/photo-1612151855475-877969f4a6cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+        title="مزایده اول"
+        city="تهران"
+        companyName="وزارت نیرو"
+        date="1401/05/02"
+        price="500,000,000"
+        remainingDay="20"
+        tags={["عمرانی", "فوری", "پیش پرداختی", "دولتی"]}
+      />
+      <ActionCard
+        imgSrc="https://images.unsplash.com/photo-1612151855475-877969f4a6cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+        title="مزایده اول"
+        city="تهران"
+        companyName="وزارت نیرو"
+        date="1401/05/02"
+        price="500,000,000"
+        remainingDay="20"
+        tags={["عمرانی", "فوری", "پیش پرداختی", "دولتی"]}
+      />
     </div>
   );
 };
