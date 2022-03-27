4684e9ea90bd0275b0b9ac616f0429cc08e6b585
ali6600alamdari <ali.alamdari9088@gmail.com>
2022-03-27 16:20:52 +0430

    fix:warning

--- src/pages/logout.jsx
+++ src/pages/logout.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import {, useNavigate } from "react-router-dom";
 
 import { logout } from "requests/user";
 import { LOGIN } from "constant/routes";
 import { api } from "utils/api";
 
 export const Logout = () => {
-  const location = useLocation();
+
   const navigate = useNavigate();
 
   useEffect(() => {
