72add8cebb7bbd6177e42223894249a1b157b407
ali6600alamdari <ali.alamdari9088@gmail.com>
2022-07-06 12:00:38 +0430

    fixLuseeffect

--- src/pages/__Test__/create_auction_test.test.jsx
+++ src/pages/__Test__/create_auction_test.test.jsx
@@ -19,62 +19,62 @@ describe("create auction component test", () => {
     expect(userNameInput.value).toBe("0");
   });
   it("Change name input", async () => {
-    render(<CreateAuctionTest />);
+    render(<CreateAuctionTest />, { flushUseEffects: false });
     const userNameInput = screen.getByTestId("nameInp");
     fireEvent.change(userNameInput, { target: { value: "Ali" } });
     expect(userNameInput.value).toBe("Ali");
   });
-  // it("Change category input", async () => {
-  //   render(<CreateAuctionTest />);
-  //   const userNameInput = screen.getByTestId("CatgorySelect");
-  //   fireEvent.change(userNameInput, { target: { value: "Honari" } });
-  //   expect(userNameInput.value).toBe("");
-  // });
+  it("Change category input", async () => {
+    render(<CreateAuctionTest />, { flushUseEffects: false });
+    const userNameInput = screen.getByTestId("CatgorySelect");
+    fireEvent.change(userNameInput, { target: { value: "Honari" } });
+    expect(userNameInput.value).toBe("");
+  });
   it("Change price input", async () => {
-    render(<CreateAuctionTest />);
+    render(<CreateAuctionTest />, { flushUseEffects: false });
     const userNameInput = screen.getByTestId("PriceInp");
     fireEvent.change(userNameInput, { target: { value: 250000 } });
     expect(userNameInput.value).toBe("250000");
   });
   it("Change startDate input", async () => {
-    render(<CreateAuctionTest />);
+    render(<CreateAuctionTest />, { flushUseEffects: false });
     const userNameInput = screen.getByTestId("PriceInp");
     fireEvent.change(userNameInput, { target: { value: "1401/02/01" } });
     expect(userNameInput.value).toBe("1401/02/01");
   });
   it("Change endDate input", async () => {
-    render(<CreateAuctionTest />);
+    render(<CreateAuctionTest />, { flushUseEffects: false });
     const userNameInput = screen.getByTestId("startDate");
     fireEvent.change(userNameInput, { target: { value: "1401/02/01" } });
     expect(userNameInput.value).toBe("");
   });
   it("Change taginput input", async () => {
-    render(<CreateAuctionTest />);
+    render(<CreateAuctionTest />, { flushUseEffects: false });
     const userNameInput = screen.getByTestId("startDate");
     fireEvent.change(userNameInput, { target: { value: "ali" } });
     expect(userNameInput.value).toBe("");
   });
   it("Change modal", async () => {
-    render(<CreateAuctionTest />);
+    render(<CreateAuctionTest />, { flushUseEffects: false });
     const userNameInput = screen.getByTestId("modalOpen");
     fireEvent.click(userNameInput);
     expect(screen.getByTestId("modalContent")).toBeInTheDocument();
   });
   it("Change 2modal", async () => {
-    render(<CreateAuctionTest />);
+    render(<CreateAuctionTest />, { flushUseEffects: false });
     const userNameInput = screen.getByTestId("2modal");
     fireEvent.click(userNameInput);
     expect(screen.getByTestId("modalContent2")).toBeInTheDocument();
   });
   it("Change close modal", async () => {
-    render(<CreateAuctionTest />);
+    render(<CreateAuctionTest />, { flushUseEffects: false });
     const btn = screen.getByTestId("subBtn");
-    // fireEvent.click(btn);
+    fireEvent.click(btn);
     expect(screen.queryByTestId("modalContent")).toBeNull();
     expect(screen.queryByTestId("modalContent2")).toBeNull();
   });
   it("icon existance", async () => {
-    render(<CreateAuctionTest />);
+    render(<CreateAuctionTest />, { flushUseEffects: false });
     const icon = screen.getByTestId("icon");
     expect(icon).toBeInTheDocument();
   });
