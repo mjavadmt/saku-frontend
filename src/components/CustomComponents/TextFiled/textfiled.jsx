7f41855d0b1d1b5977843639a7e947a017f4424a
Sara Younesi <sarayounesi.iust@gmail.com>
2023-04-19 21:58:11 +0330

    CustomButton component

---
+++ src/components/CustomComponents/TextFiled/textfiled.jsx
@@ -0,0 +1,27 @@
+import { TextField } from "@mui/material";
+import { useState } from "react";
+
+export const CustomTextF = ({ ...attributes }) => {
+    const [userData, setUserData] = useState({});
+    const [provinceId, setProvinceId] = useState(-1);
+    const handleChange = (e, val) => {
+        let data = { ...userData };
+        data[e.target.name] = e.target.value;
+        if (e.target.name === "province") {
+            setProvinceId(val.props.id);
+            data["city"] = "";
+        }
+        setUserData(data);
+    };
+
+    return (
+        <TextField
+            fullWidth
+            multiline
+            onChange={handleChange}
+            required
+            variant='outlined'
+            {...attributes}
+        />
+    );
+};
