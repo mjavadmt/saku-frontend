57351b3fc5a7779f23a396f8e79946c2340f1e37
Amirhossein Roudgar <2000Amir1379@gmail.com>
2023-05-02 15:33:57 +0330

    updated host to server ip address

--- src/utils/config.jsx
+++ src/utils/config.jsx
@@ -1,2 +1,3 @@
 // export const host = process.env.REACT_APP_HOST;
-export const host = 'http://localhost:8888/';
+// export const host = 'http://localhost:8888/';
+export const host = 'http://188.121.97.132:8888/';
