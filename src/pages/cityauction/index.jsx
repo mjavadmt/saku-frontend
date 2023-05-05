2c1fc48b600b7b4a177012ecd84972877921d93a
Sara Younesi <sarayounesi.iust@gmail.com>
2023-05-05 23:43:56 +0330

    feat:city-auction/ add dynamic API and logic to cart and add city field to craet my auction

--- src/pages/cityauction/index.jsx
+++ src/pages/cityauction/index.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState, useRef } from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -7,8 +8,12 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Grid } from "@mui/material";
 import "./index.css";
+import axios, { post } from "axios";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
+import { useEffect } from "react";
+import { host } from "utils/config";
 //مزایده مناقصه بر اساس شهر ها
+
 const bull = (
     <Box
         component='span'
@@ -19,279 +24,62 @@ const bull = (
 );
 
 export const CityAuction = ({}) => {
+    const token = `Bearer ${localStorage.getItem("access")}`;
+    const [auctionCity, setAuctionCity] = useState([]);
+    const [city, setCity] = useState("");
+    useEffect(() => {
+        axios
+            .get(`${host}auction/city/`, {
+                headers: {
+                    "Content-Type": "application/json ",
+                    Authorization: token,
+                },
+            })
+            .then((res) => {
+                console.log(res.data);
+                setAuctionCity(res.data);
+            });
+    }, []);
     return (
         <Grid container>
-            <Grid item>
-                <Card sx={{ minWidth: 265 }} className='card1'>
-                    <CardContent>
-                        <Typography
-                            variant='h5'
-                            component='div'
-                            className='city'
-                        >
-                            تهران
-                        </Typography>
+            {auctionCity.map((city) => {
+                return (
+                    <Grid item>
+                        <Card sx={{ minWidth: 265 }} className='card1'>
+                            <CardContent>
+                                <Typography
+                                    variant='h5'
+                                    component='div'
+                                    className='city'
+                                >
+                                    {city.name}{" "}
+                                </Typography>
 
-                        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                            مزایده مناقصه تهران
-                        </Typography>
-                        <Typography variant='body2'>1234 آگهی</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button
-                            size='small'
-                            style={{ color: "#c786d3", marginRight: "140px" }}
-                        >
-                            مشاهده بیشتر <KeyboardBackspaceIcon />{" "}
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
-            <Grid item>
-                <Card sx={{ minWidth: 265 }} className='card1'>
-                    <CardContent>
-                        <Typography variant='h5' component='div'>
-                            اهواز
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                            مزایده مناقصه اهواز{" "}
-                        </Typography>
-                        <Typography variant='body2'>324 آگهی</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button
-                            size='small'
-                            style={{ color: "#c786d3", marginRight: "140px" }}
-                        >
-                            مشاهده بیشتر <KeyboardBackspaceIcon />{" "}
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
-            <Grid item>
-                <Card sx={{ minWidth: 265 }} className='card1'>
-                    <CardContent>
-                        <Typography variant='h5' component='div'>
-                            مشهد
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                            مزایده مناقصه مشهد
-                        </Typography>
-                        <Typography variant='body2'>7659 آگهی</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button
-                            size='small'
-                            style={{ color: "#c786d3", marginRight: "140px" }}
-                        >
-                            مشاهده بیشتر <KeyboardBackspaceIcon />{" "}
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
-            <Grid item>
-                <Card sx={{ minWidth: 265 }} className='card1'>
-                    <CardContent>
-                        <Typography variant='h5' component='div'>
-                            اصفهان
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                            مزایده مناقصه اصفهان
-                        </Typography>
-                        <Typography variant='body2'>3456 آگهی</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size='small' style={{ color: "#c786d3" }}>
-                            مشاهده بیشتر <KeyboardBackspaceIcon />
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
-            <Grid item>
-                <Card sx={{ minWidth: 265 }} className='card1'>
-                    <CardContent>
-                        <Typography
-                            variant='h5'
-                            component='div'
-                            className='city'
-                        >
-                            شیراز{" "}
-                        </Typography>
-
-                        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                            مزایده مناقصه شیراز{" "}
-                        </Typography>
-                        <Typography variant='body2'>1234 آگهی</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button
-                            size='small'
-                            style={{ color: "#c786d3", marginRight: "140px" }}
-                        >
-                            مشاهده بیشتر <KeyboardBackspaceIcon />{" "}
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
-            <Grid item>
-                <Card sx={{ minWidth: 265 }} className='card1'>
-                    <CardContent>
-                        <Typography variant='h5' component='div'>
-                            اراک{" "}
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                            مزایده مناقصه اراک{" "}
-                        </Typography>
-                        <Typography variant='body2'>324 آگهی</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button
-                            size='small'
-                            style={{ color: "#c786d3", marginRight: "140px" }}
-                        >
-                            مشاهده بیشتر <KeyboardBackspaceIcon />{" "}
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
-            <Grid item>
-                <Card sx={{ minWidth: 265 }} className='card1'>
-                    <CardContent>
-                        <Typography variant='h5' component='div'>
-                            یزد
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                            مزایده مناقصه یزد{" "}
-                        </Typography>
-                        <Typography variant='body2'>7659 آگهی</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button
-                            size='small'
-                            style={{ color: "#c786d3", marginRight: "140px" }}
-                        >
-                            مشاهده بیشتر <KeyboardBackspaceIcon />{" "}
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
-            <Grid item>
-                <Card sx={{ minWidth: 265 }} className='card1'>
-                    <CardContent>
-                        <Typography variant='h5' component='div'>
-                            ساری
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                            مزایده مناقصه ساری{" "}
-                        </Typography>
-                        <Typography variant='body2'>3456 آگهی</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button
-                            size='small'
-                            style={{ color: "#c786d3", marginRight: "140px" }}
-                        >
-                            مشاهده بیشتر <KeyboardBackspaceIcon />
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
-            <Grid item>
-                <Card sx={{ minWidth: 265 }} className='card1'>
-                    <CardContent>
-                        <Typography
-                            variant='h5'
-                            component='div'
-                            className='city'
-                        >
-                            تهران
-                        </Typography>
-
-                        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                            مزایده مناقصه تهران
-                        </Typography>
-                        <Typography variant='body2'>1234 آگهی</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button
-                            size='small'
-                            style={{ color: "#c786d3", marginRight: "140px" }}
-                        >
-                            مشاهده بیشتر <KeyboardBackspaceIcon />{" "}
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
-            <Grid item>
-                <Card sx={{ minWidth: 265 }} className='card1'>
-                    <CardContent>
-                        {/* <Typography
-                            sx={{ fontSize: 14 }}
-                            color='text.secondary'
-                            gutterBottom
-                        >
-                            Word of the Day
-                        </Typography> */}
-                        <Typography variant='h5' component='div'>
-                            اهواز
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                            مزایده مناقصه اهواز{" "}
-                        </Typography>
-                        <Typography variant='body2'>324 آگهی</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button
-                            size='small'
-                            style={{ color: "#c786d3", marginRight: "140px" }}
-                        >
-                            مشاهده بیشتر <KeyboardBackspaceIcon />{" "}
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
-            <Grid item>
-                <Card sx={{ minWidth: 265 }} className='card1'>
-                    <CardContent>
-                        <Typography variant='h5' component='div'>
-                            مشهد
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                            مزایده مناقصه مشهد
-                        </Typography>
-                        <Typography variant='body2'>7659 آگهی</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button
-                            size='small'
-                            style={{ color: "#c786d3", marginRight: "140px" }}
-                        >
-                            مشاهده بیشتر <KeyboardBackspaceIcon />{" "}
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
-            <Grid item>
-                <Card sx={{ minWidth: 265 }} className='card1'>
-                    <CardContent>
-                        <Typography variant='h5' component='div'>
-                            اصفهان
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                            مزایده مناقصه اصفهان
-                        </Typography>
-                        <Typography variant='body2'>3456 آگهی</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button
-                            size='small'
-                            style={{ color: "#c786d3", marginRight: "140px" }}
-                        >
-                            مشاهده بیشتر <KeyboardBackspaceIcon />
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
+                                <Typography
+                                    sx={{ mb: 1.5 }}
+                                    color='text.secondary'
+                                >
+                                    مزایده مناقصه {city.name}{" "}
+                                </Typography>
+                                <Typography variant='body2'>
+                                    1234 آگهی
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button
+                                    size='small'
+                                    style={{
+                                        color: "#c786d3",
+                                        marginRight: "140px",
+                                    }}
+                                >
+                                    مشاهده بیشتر <KeyboardBackspaceIcon />{" "}
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                );
+            })}
         </Grid>
     );
 };
