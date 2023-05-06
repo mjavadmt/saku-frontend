501dd032a786ab300d152ad5e1c969bf97b47dd6
Sara Younesi <sarayounesi.iust@gmail.com>
2023-05-06 12:34:58 +0330

    fix:category/Correct redirection and bind API

--- src/pages/auctionPage/index.jsx
+++ src/pages/auctionPage/index.jsx
@@ -7,118 +7,147 @@ import Pagination from "@mui/material/Pagination";
 import { CircularProgress } from "@mui/material";
 import noAuctionImage from "assets/img/no-auction-image-2.svg";
 import {
-  getAllAuctions,
-  getFilteredAuctions,
+    getAllAuctions,
+    getFilteredAuctions,
 } from "utils/api/requests/myAuctions";
 import { useDispatch, useSelector } from "react-redux";
 import { END_LOADING, SET_PAGE, ALL_AUCTIONS } from "constants/actionTypes";
-import { getallAuctoins, getfilteredAuctoin } from "actions/auctions";
+import { useNavigate, useLocation } from "react-router-dom";
+import {
+    getallAuctoins,
+    getallCity,
+    getfilteredAuctoin,
+} from "actions/auctions";
 
 export const AuctionPage = () => {
-  const { auctions, page, name, basePrice, isLoading } = useSelector((data) => {
-    return data.auction;
-  });
-  const dataOnPage = 5;
-  const dispatch = useDispatch();
-  const [type, setType] = React.useState("");
-  const [status, setStatus] = React.useState("");
-  const [tag, setTag] = React.useState([]);
-  const [is_online, setIs_online] = React.useState("");
-  const [description, setDescription] = React.useState("");
-  const [category, setCategory] = React.useState("");
-  const handleChange = (event, value) => {
-    dispatch({ type: SET_PAGE, payload: { page: value } });
-  };
-  const paginatedData = () => {
-    let currentItem = (page - 1) * dataOnPage;
-    return auctions?.slice(currentItem, currentItem + dataOnPage);
-  };
-  const filterSubmited = async () => {
-    dispatch({ type: SET_PAGE, payload: { page: 1 } });
-    let filteredObj = {};
-    if (name !== "") filteredObj["name"] = name;
-    if (type !== "") filteredObj["mode"] = type;
-    if (basePrice !== "") filteredObj["limit"] = basePrice;
-    if (tag !== "") filteredObj["tags"] = tag.join(",");
-    console.log("is Online is :", is_online);
-    if (is_online || is_online == "0") filteredObj["is_online"] = is_online;
-    if (description !== "") filteredObj["desc"] = description;
-    if (category !== "") filteredObj["category"] = category;
-    dispatch(getfilteredAuctoin(filteredObj, `${GET_ALL_AUCTIONS}`));
-    dispatch({ type: END_LOADING });
-  };
-  useEffect(() => {
-    dispatch(getallAuctoins(`${GET_ALL_AUCTIONS}`));
-    dispatch({ type: END_LOADING });
-  }, []);
-  return (
-    <div>
-      <Filtering
-        hasRadioBtn={true}
-        type={type}
-        setType={setType}
-        status={status}
-        setStatus={setStatus}
-        tag={tag}
-        setTag={setTag}
-        is_online={is_online}
-        setIs_online={setIs_online}
-        description={description}
-        setDescription={setDescription}
-        category={category}
-        setCategory={setCategory}
-        name={name}
-        basePrice={basePrice}
-        filterSubmited={filterSubmited}
-      />
-      {isLoading ? (
-        <span className="flex h-full justify-center items-center mt-16  ">
-          <CircularProgress color="inherit" />
-        </span>
-      ) : (
-        <>
-          {auctions?.length > 0 && (
-            <>
-              {paginatedData()?.map((auction) => (
-                <ActionCard
-                  imgSrc={
-                    !!auction.auction_image
-                      ? auction.auction_image
-                      : noAuctionImage
-                  }
-                  ourImage={!!auction.auction_image}
-                  title={auction.name}
-                  city={auction.location}
-                  companyName={auction.user.username}
-                  date={new Date(auction.finished_at).toLocaleDateString(
-                    "fa-IR"
-                  )}
-                  price={auction.limit}
-                  mode={auction.mode}
-                  isOnline={auction.is_online}
-                  remainingDay={Math.ceil(
-                    (new Date(auction.finished_at).getTime() -
-                      new Date().getTime()) /
-                      (1000 * 3600 * 24)
-                  )}
-                  tags={[...auction.tags.map((tag) => tag.name)]}
-                  id={1}
-                  token={auction.token}
-                />
-              ))}
+    const { auctions, page, name, basePrice, isLoading } = useSelector(
+        (data) => {
+            return data.auction;
+        }
+    );
+    const dataOnPage = 5;
+    const dispatch = useDispatch();
+    const [type, setType] = React.useState("");
+    const [status, setStatus] = React.useState("");
+    const [tag, setTag] = React.useState([]);
+    const [is_online, setIs_online] = React.useState("");
+    const [description, setDescription] = React.useState("");
+    const [category, setCategory] = React.useState("");
+    const location = useLocation();
+    const handleChange = (event, value) => {
+        dispatch({ type: SET_PAGE, payload: { page: value } });
+    };
+    const paginatedData = () => {
+        let currentItem = (page - 1) * dataOnPage;
+        return auctions?.slice(currentItem, currentItem + dataOnPage);
+    };
+    const filterSubmited = async () => {
+        dispatch({ type: SET_PAGE, payload: { page: 1 } });
+        let filteredObj = {};
+        if (name !== "") filteredObj["name"] = name;
+        if (type !== "") filteredObj["mode"] = type;
+        if (basePrice !== "") filteredObj["limit"] = basePrice;
+        if (tag !== "") filteredObj["tags"] = tag.join(",");
+        console.log("is Online is :", is_online);
+        if (is_online || is_online == "0") filteredObj["is_online"] = is_online;
+        if (description !== "") filteredObj["desc"] = description;
+        if (category !== "") filteredObj["category"] = category;
+        console.log("filterd obj", category);
+        dispatch(getfilteredAuctoin(filteredObj, `${GET_ALL_AUCTIONS}`));
+        dispatch({ type: END_LOADING });
+    };
 
-              <div className="flex justify-center mt-5">
-                <Pagination
-                  count={Math.ceil(auctions?.length / dataOnPage)}
-                  page={page}
-                  onChange={handleChange}
-                  color="primary"
-                />
-              </div>
-            </>
-          )}
-        </>
-      )}
-    </div>
-  );
+    useEffect(() => {
+        if (category) filterSubmited();
+    }, [category]);
+
+    useEffect(() => {
+        console.log("loc", location);
+        if (location?.state?.catId) {
+            console.log("saalllllllam", location?.state?.catId);
+            setCategory(location?.state?.catId);
+
+            dispatch({ type: END_LOADING });
+        } else {
+            dispatch(getallAuctoins(`${GET_ALL_AUCTIONS}`));
+            dispatch({ type: END_LOADING });
+        }
+    }, []);
+
+    return (
+        <div>
+            <Filtering
+                hasRadioBtn={true}
+                type={type}
+                setType={setType}
+                status={status}
+                setStatus={setStatus}
+                tag={tag}
+                setTag={setTag}
+                is_online={is_online}
+                setIs_online={setIs_online}
+                description={description}
+                setDescription={setDescription}
+                category={category}
+                setCategory={setCategory}
+                name={name}
+                basePrice={basePrice}
+                filterSubmited={filterSubmited}
+            />
+            {isLoading ? (
+                <span className='flex h-full justify-center items-center mt-16  '>
+                    <CircularProgress color='inherit' />
+                </span>
+            ) : (
+                <>
+                    {auctions?.length > 0 && (
+                        <>
+                            {paginatedData()?.map((auction) => (
+                                <ActionCard
+                                    imgSrc={
+                                        !!auction.auction_image
+                                            ? auction.auction_image
+                                            : noAuctionImage
+                                    }
+                                    ourImage={!!auction.auction_image}
+                                    title={auction.name}
+                                    city={auction.location}
+                                    companyName={auction.user.username}
+                                    date={new Date(
+                                        auction.finished_at
+                                    ).toLocaleDateString("fa-IR")}
+                                    price={auction.limit}
+                                    mode={auction.mode}
+                                    isOnline={auction.is_online}
+                                    remainingDay={Math.ceil(
+                                        (new Date(
+                                            auction.finished_at
+                                        ).getTime() -
+                                            new Date().getTime()) /
+                                            (1000 * 3600 * 24)
+                                    )}
+                                    tags={[
+                                        ...auction.tags.map((tag) => tag.name),
+                                    ]}
+                                    id={1}
+                                    token={auction.token}
+                                />
+                            ))}
+
+                            <div className='flex justify-center mt-5'>
+                                <Pagination
+                                    count={Math.ceil(
+                                        auctions?.length / dataOnPage
+                                    )}
+                                    page={page}
+                                    onChange={handleChange}
+                                    color='primary'
+                                />
+                            </div>
+                        </>
+                    )}
+                </>
+            )}
+        </div>
+    );
 };
--- src/pages/category/index.jsx
+++ src/pages/category/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Grid, Paper, Typography } from "@mui/material";
 import "./index.css";
 import { Slide } from "react-slideshow-image";
@@ -8,6 +8,9 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import "./index.css";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
+import { useNavigate } from "react-router-dom";
+import { getCategories } from "utils/api/requests/createAuction";
+import { GET_CATEGORIES } from "utils/constant/apiRoutes";
 
 const slideImages = [
     "https://dummyimage.com/600x400/b1e6b2/1c2ae8.png",
@@ -23,6 +26,17 @@ const properties = {
     arrows: true,
 };
 export const Category = () => {
+    const [categories, setCategories] = useState([]);
+    const navigate = useNavigate();
+    useEffect(() => {
+        async function fetchData() {
+            const getCategoriesRes = await getCategories(GET_CATEGORIES);
+            if (getCategoriesRes && getCategoriesRes.status === 200) {
+                setCategories(getCategoriesRes.data);
+            }
+        }
+        fetchData();
+    }, []);
     return (
         <div>
             <div className='text'>مزایده / مناقصه بر اساس دسته بندی </div>
@@ -45,157 +59,48 @@ export const Category = () => {
             </Slide>
 
             <Grid container>
-                <Grid item>
-                    <Card sx={{ minWidth: 330 , minHeight:230 }} className='card1'>
-                        <CardContent>
-                            {/* <Typography
-                                variant='h6'
-                                component='div'
-                                className='city'
+                {categories.map((cat) => {
+                    return (
+                        <Grid item>
+                            <Card
+                                sx={{ minWidth: 330, minHeight: 230 }}
+                                className='card1'
                             >
-                                تهران
-                            </Typography> */}
-
-                        </CardContent>
-                        <CardActions>
-                            <Button
-                                size='small'
-                                style={{
-                                    color: "white",
-                                  marginRight:"98px"
-                                }}
-                            >
-                                       <Typography
-                                variant='h6'
-                                component='div'
-                                className='city'
-                            >
-                                تهران
-                            </Typography>                          </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid item>
-                    <Card sx={{minWidth: 330 , minHeight:230 }} className='card1'>
-                        <CardContent>
-                            <Typography variant='h5' component='div'>
-                                اهواز
-                            </Typography>
-                            <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                                مزایده مناقصه اهواز{" "}
-                            </Typography>
-                            <Typography variant='body2'>324 آگهی</Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button
-                                size='small'
-                                style={{
-                                    color: "#c786d3",
-                                    marginRight: "140px",
-                                }}
-                            >
-                                مشاهده بیشتر <KeyboardBackspaceIcon />{" "}
-                            </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid item>
-                    <Card sx={{ minWidth: 330 , minHeight:230 }} className='card1'>
-                        <CardContent>
-                            <Typography variant='h5' component='div'>
-                                مشهد
-                            </Typography>
-                            <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                                مزایده مناقصه مشهد
-                            </Typography>
-                            <Typography variant='body2'>7659 آگهی</Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button
-                                size='small'
-                                style={{
-                                    color: "#c786d3",
-                                    marginRight: "140px",
-                                }}
-                            >
-                                مشاهده بیشتر <KeyboardBackspaceIcon />{" "}
-                            </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid item>
-                    <Card sx={{ minWidth: 330 , minHeight:230 }} className='card1'>
-                        <CardContent>
-                            <Typography variant='h5' component='div'>
-                                اصفهان
-                            </Typography>
-                            <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                                مزایده مناقصه اصفهان
-                            </Typography>
-                            <Typography variant='body2'>3456 آگهی</Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size='small' style={{ color: "#c786d3" }}>
-                                مشاهده بیشتر <KeyboardBackspaceIcon />
-                            </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid item>
-                    <Card sx={{ minWidth: 330 , minHeight:230 }} className='card1'>
-                        <CardContent>
-                            <Typography
-                                variant='h5'
-                                component='div'
-                                className='city'
-                            >
-                                شیراز{" "}
-                            </Typography>
-
-                            <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                                مزایده مناقصه شیراز{" "}
-                            </Typography>
-                            <Typography variant='body2'>1234 آگهی</Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button
-                                size='small'
-                                style={{
-                                    color: "#c786d3",
-                                    marginRight: "140px",
-                                }}
-                            >
-                                مشاهده بیشتر <KeyboardBackspaceIcon />{" "}
-                            </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-                <Grid item>
-                    <Card sx={{ minWidth: 330 , minHeight:230 }} className='card1'>
-                        <CardContent>
-                            <Typography variant='h5' component='div'>
-                                اراک{" "}
-                            </Typography>
-                            <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                                مزایده مناقصه اراک{" "}
-                            </Typography>
-                            <Typography variant='body2'>324 آگهی</Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button
-                                size='small'
-                                style={{
-                                    color: "#c786d3",
-                                    marginRight: "140px",
-                                }}
-                            >
-                                مشاهده بیشتر <KeyboardBackspaceIcon />{" "}
-                            </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-       
-      
+                                <CardContent>
+                                    {/* <Typography
+                                    variant='h6'
+                                    component='div'
+                                    className='city'
+                                >
+                                    تهران
+                                </Typography> */}
+                                </CardContent>
+                                <CardActions>
+                                    <Button
+                                        size='small'
+                                        style={{
+                                            color: "white",
+                                            marginRight: "98px",
+                                        }}
+                                        onClick={() => {
+                                            navigate("/auction-page", {
+                                                state: { catId: cat.name },
+                                            });
+                                        }}
+                                    >
+                                        <Typography
+                                            variant='h6'
+                                            component='div'
+                                            className='city'
+                                        >
+                                            {cat.name}
+                                        </Typography>{" "}
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    );
+                })}
             </Grid>
         </div>
     );
