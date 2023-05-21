b70dc19448312a10cdac4b2491b93f0bc478cceb
sara_younesi <sarayounesi.iust@gmail.com>
2023-05-21 18:13:34 +0330

    fix:card/add button to card

--- src/components/MyAuction/card.jsx
+++ src/components/MyAuction/card.jsx
@@ -3,14 +3,29 @@ import AttachMoneyRoundedIcon from "@mui/icons-material/AttachMoneyRounded";
 import PeopleOutlineRoundedIcon from "@mui/icons-material/PeopleOutlineRounded";
 import CategoryRoundedIcon from "@mui/icons-material/CategoryRounded";
 import GavelRoundedIcon from "@mui/icons-material/GavelRounded";
-import React from "react";
+import React, { useState } from "react";
 import { defineUnit } from "utils/formatPrice";
 import { useNavigate } from "react-router-dom";
 import { AUCTION_DETAIL_WITHOUT_SUFFIX } from "utils/constant/routes";
 import "./styles.scss";
 import noAuctionImage from "assets/img/no-auction-image-2.svg";
 import cx from "classnames";
-
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import Stack from "@mui/material/Stack";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+// import Transition from "@mui/material/Transition";
 const defineStatus = (startDate, endDate) => {
     if (new Date(startDate) > new Date() && new Date(endDate) < new Date())
         return (
@@ -43,12 +58,52 @@ export const Card = ({
     limit,
     token,
 }) => {
+    const [openAlart, setOpenAlart] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
+    const [value, setValue] = useState([20, 37]);
+    const [sortBy, setSortBy] = useState(10);
+    const [auctionName, setAuctionName] = useState("");
+    const handleChangeRadio = (event, identifier) => {
+        // if (identifier === "type") onChangeType(event.target.value);
+        // else onChangeStatus(event.target.value);
+    };
+    const handleClickOpen = (e) => {
+        if (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            setOpen(true);
+        }
+    };
+
+    const handleClose = (e) => {
+        if (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            setOpen(false);
+        }
+    };
+
+    const handleClickAlart = () => {
+        setOpen(true);
+    };
+
+    const handleCloseAlart = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+
+        setOpenAlart(false);
+    };
     let navigate = useNavigate();
     return (
         <div
-            onClick={() =>
-                navigate(`${AUCTION_DETAIL_WITHOUT_SUFFIX}/${token}`)
-            }
+            onClick={(e) => {
+                if (e) {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    navigate(`${AUCTION_DETAIL_WITHOUT_SUFFIX}/${token}`);
+                }
+            }}
             className='bg-cardColor rounded-3xl cursor-pointer my-auction-card'
         >
             <img
@@ -97,6 +152,216 @@ export const Card = ({
                     <p className='text-sm font-bold'>قیمت : ‌</p>
                     <p className='text-sm'>{defineUnit(limit, 1)}</p>
                 </div>
+            </div>
+            <div>
+                <Button variant='outlined' onClick={(e) => handleClickOpen(e)}>
+                    ثبت نظر
+                </Button>
+                <Dialog
+                    open={open}
+                    // TransitionComponent={Transition}
+                    keepMounted
+                    onClose={handleClose}
+                    aria-describedby='alert-dialog-slide-description'
+                >
+                    <DialogTitle>{"فرم نظرسنجی"}</DialogTitle>
+                    <DialogContent>
+                        <FormControl>
+                            <FormLabel id='demo-row-radio-buttons-group-label'>
+                                1.نسبت ارزش به هزینه در این مزایده مناقصه معقول
+                                بود:
+                            </FormLabel>
+                            <RadioGroup
+                                row
+                                aria-labelledby='demo-row-radio-buttons-group-label'
+                                name='row-radio-buttons-group'
+                            >
+                                <FormControlLabel
+                                    value='best'
+                                    control={<Radio />}
+                                    label='عالی'
+                                />
+                                <FormControlLabel
+                                    value='good'
+                                    control={<Radio />}
+                                    label='خوب'
+                                />
+                                <FormControlLabel
+                                    value='normal'
+                                    control={<Radio />}
+                                    label='متوسط'
+                                />
+                                <FormControlLabel
+                                    value='bad'
+                                    control={<Radio />}
+                                    label='ضعیف'
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                    </DialogContent>
+                    <DialogContent>
+                        <FormControl>
+                            <FormLabel id='demo-row-radio-buttons-group-label'>
+                                2.میزان در دسترس پذیری فرد مناقصه/مزایده گزار:
+                            </FormLabel>
+                            <RadioGroup
+                                row
+                                aria-labelledby='demo-row-radio-buttons-group-label'
+                                name='row-radio-buttons-group'
+                            >
+                                <FormControlLabel
+                                    value='best'
+                                    control={<Radio />}
+                                    label='عالی'
+                                />
+                                <FormControlLabel
+                                    value='good'
+                                    control={<Radio />}
+                                    label='خوب'
+                                />
+                                <FormControlLabel
+                                    value='normal'
+                                    control={<Radio />}
+                                    label='متوسط'
+                                />
+                                <FormControlLabel
+                                    value='bad'
+                                    control={<Radio />}
+                                    label='ضعیف'
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                    </DialogContent>
+                    <DialogContent>
+                        <FormControl>
+                            <FormLabel id='demo-row-radio-buttons-group-label'>
+                                3. پکیج مزایده/مناقصه کامل و گویا بود:
+                            </FormLabel>
+                            <RadioGroup
+                                row
+                                aria-labelledby='demo-row-radio-buttons-group-label'
+                                name='row-radio-buttons-group'
+                            >
+                                <FormControlLabel
+                                    value='best'
+                                    control={<Radio />}
+                                    label='عالی'
+                                />
+                                <FormControlLabel
+                                    value='good'
+                                    control={<Radio />}
+                                    label='خوب'
+                                />
+                                <FormControlLabel
+                                    value='normal'
+                                    control={<Radio />}
+                                    label='متوسط'
+                                />
+                                <FormControlLabel
+                                    value='bad'
+                                    control={<Radio />}
+                                    label='ضعیف'
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                    </DialogContent>
+                    <DialogContent>
+                        <FormControl>
+                            <FormLabel id='demo-row-radio-buttons-group-label'>
+                                4.زمان، مکان و تاریخ گشایش پیشنهاد مناسب بود:
+                            </FormLabel>
+                            <RadioGroup
+                                row
+                                aria-labelledby='demo-row-radio-buttons-group-label'
+                                name='row-radio-buttons-group'
+                            >
+                                <FormControlLabel
+                                    value='best'
+                                    control={<Radio />}
+                                    label='عالی'
+                                />
+                                <FormControlLabel
+                                    value='good'
+                                    control={<Radio />}
+                                    label='خوب'
+                                />
+                                <FormControlLabel
+                                    value='normal'
+                                    control={<Radio />}
+                                    label='متوسط'
+                                />
+                                <FormControlLabel
+                                    value='bad'
+                                    control={<Radio />}
+                                    label='ضعیف'
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                    </DialogContent>
+                    <DialogContent>
+                        <FormControl>
+                            <FormLabel id='demo-row-radio-buttons-group-label'>
+                                5. نیازهای شما از طریق این مزایده/مناقصه برآورده
+                                شد:
+                            </FormLabel>
+                            <RadioGroup
+                                row
+                                aria-labelledby='demo-row-radio-buttons-group-label'
+                                name='row-radio-buttons-group'
+                            >
+                                <FormControlLabel
+                                    value='best'
+                                    control={<Radio />}
+                                    label='عالی'
+                                />
+                                <FormControlLabel
+                                    value='good'
+                                    control={<Radio />}
+                                    label='خوب'
+                                />
+                                <FormControlLabel
+                                    value='normal'
+                                    control={<Radio />}
+                                    label='متوسط'
+                                />
+                                <FormControlLabel
+                                    value='bad'
+                                    control={<Radio />}
+                                    label='ضعیف'
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button
+                            onClick={(e) => handleClose(e)}
+                            style={{ color: "#f06292" }}
+                        >
+                            لغو
+                        </Button>
+                        <Stack>
+                            <Button
+                                onClick={handleClickAlart}
+                                style={{ color: "#1de9b6" }}
+                            >
+                                ثبت
+                            </Button>
+                            <Snackbar
+                                open={openAlart}
+                                autoHideDuration={3000}
+                                onClose={handleCloseAlart}
+                            >
+                                <Alert
+                                    onClose={handleCloseAlart}
+                                    severity='success'
+                                    sx={{ width: "100%" }}
+                                >
+                                    نظر شما با موفقیت ثبت شد.
+                                </Alert>
+                            </Snackbar>
+                        </Stack>
+                    </DialogActions>
+                </Dialog>
             </div>
         </div>
     );
