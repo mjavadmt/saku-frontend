8e4fd6ba8ed90528dbed9ad85c86eb5562c31320
Amirhossein Roudgar <2000Amir1379@gmail.com>
2023-07-01 22:45:51 +0330

    fix: revert proxy default settings

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,14 @@
-// const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-// module.exports = function (app) {
-//   app.use(
-//     "/api",
-//     createProxyMiddleware({
-//       target: "http://188.121.110.151:8888/",
-//     //   target: "http://localhost:3000",
-//       changeOrigin: true,
-//       pathRewrite: { "^/api": "" },
-//       secure: false,
-//     })
-//   );
-// };
+module.exports = function (app) {
+  app.use(
+    "/saku**",
+    createProxyMiddleware({
+      target: "http://188.121.110.151:8888/",
+    //   target: "http://localhost:3000",
+      changeOrigin: true,
+      pathRewrite: { "^/saku": "" },
+      secure: false,
+    })
+  );
+};
