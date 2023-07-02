1bc20bd27eb05cb39010ade89245badee8503600
Amirhossein Roudgar <2000Amir1379@gmail.com>
2023-07-03 00:43:45 +0330

    fix: updated proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,13 +2,13 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    // "/saku**",
-    "http://188.121.113.13:8888/",
+    "/saku/",
+    // "http://188.121.113.13:8888/",
     createProxyMiddleware({
       target: "http://188.121.113.13:8888/",
-    //   target: "http://localhost:3000",
-      // changeOrigin: true,
-      // pathRewrite: { "^/saku": "" },
+      // target: "http://localhost:3000",
+      changeOrigin: true,
+      pathRewrite: { "^/saku/": "" },
       secure: false,
     })
   );
